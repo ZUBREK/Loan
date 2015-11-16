7cee0162a347cc74f6ce5a13a9cdb4cfb7b7eac9
LFVH <luizfelipevh@gmail.com>
2015-11-16 02:34:50 -0200

    sim

--- loan/src/ifpr/model/MenuUsuariosViewMB.java
+++ loan/src/ifpr/model/MenuUsuariosViewMB.java
@@ -31,8 +31,8 @@ public class MenuUsuariosViewMB {
 
 	public MenuUsuariosViewMB() {
 		context = FacesContext.getCurrentInstance();
-		loginControllerMB = context.getApplication().evaluateExpressionGet(
-				context, "#{loginControllerMB}", LoginControllerMB.class);
+		loginControllerMB = context.getApplication().evaluateExpressionGet(context, "#{loginControllerMB}",
+				LoginControllerMB.class);
 		pessoaLogada = loginControllerMB.getPessoaLogada();
 	}
 
@@ -40,22 +40,22 @@ public class MenuUsuariosViewMB {
 	public void init() {
 		model = new DefaultMenuModel();
 		adicionarItem("Página Principal", "/index.ifpr", "", "home16", false, 0);
-		adicionarItem("Meu Perfil", "/user/home.ifpr",
-				"#{breadCrumbController.navigateHome}", "home16", false, 0);
+		adicionarItem("Meu Perfil", "/user/home.ifpr", "#{breadCrumbController.navigateHome}", "home16", false, 0);
 		if (pessoaLogada.getTipo().equals(TipoPessoa.ROLE_ADMIN)) {
-			adicionarItem("Gerenciar - Arquivos", "/adm/arquivo/arquivo.ifpr",
-					"#{breadCrumbController.navigate(1)}", "attach16", false,1);
+			adicionarItem("Gerenciar - Arquivos", "/adm/arquivo/arquivo.ifpr", "#{breadCrumbController.navigate(1)}",
+					"attach16", false, 1);
 
-			adicionarItem("Gerenciar - Campus", "/adm/campus.ifpr",
-					"#{breadCrumbController.navigate(6)}", "attach16", false, 6);
+			adicionarItem("Gerenciar - Campus", "/adm/campus.ifpr", "#{breadCrumbController.navigate(6)}", "attach16",
+					false, 6);
 
-			adicionarItem("Gerenciar - Notícias", "/adm/noticia.ifpr",
-					"#{breadCrumbController.navigate(7)}", "attach16", false, 7);
+			adicionarItem("Gerenciar - Notícias", "/adm/noticia.ifpr", "#{breadCrumbController.navigate(7)}",
+					"attach16", false, 7);
 
-			adicionarItem("Gerenciar - Projetos PEA", "/tec_adm/projeto.ifpr",
-					"#{breadCrumbController.navigate(11)}", "attach16", false, 11);
-			
-			adicionarItem("Gerenciar - Horários de Assistências", "/adm/noticia.ifpr", "#{breadCrumbController.navigate(7)}", "attach16", false, 13);
+			adicionarItem("Gerenciar - Projetos PEA", "/tec_adm/projeto.ifpr", "#{breadCrumbController.navigate(11)}",
+					"attach16", false, 11);
+
+			adicionarItem("Gerenciar - Horários de Assistências", "/adm/noticia.ifpr",
+					"#{breadCrumbController.navigate(13)}", "attach16", false, 13);
 			adicionarItensAdm();
 
 		} else if (pessoaLogada.getTipo().equals(TipoPessoa.ROLE_TEC_ESP)) {
@@ -72,34 +72,34 @@ public class MenuUsuariosViewMB {
 
 	private void adicionarItensAdm() {
 		submenu = new DefaultSubMenu("Gerenciar - Usuários");
-		adicionarItem("Tec. Administrativos", "/adm/tec_adm.ifpr",
-				"#{breadCrumbController.navigate(2)}", "user16", true, 2);
+		adicionarItem("Tec. Administrativos", "/adm/tec_adm.ifpr", "#{breadCrumbController.navigate(2)}", "user16",
+				true, 2);
 
-		adicionarItem("Tec. Esportivos", "/tec_adm/tec_esp.ifpr",
-				"#{breadCrumbController.navigate(3)}", "user16", true, 3);
+		adicionarItem("Tec. Esportivos", "/tec_adm/tec_esp.ifpr", "#{breadCrumbController.navigate(3)}", "user16", true,
+				3);
 
-		adicionarItem("Coordenadores", "/tec_adm/coordenadorPea.ifpr",
-				"#{breadCrumbController.navigate(4)}", "user16", true, 4);
+		adicionarItem("Coordenadores", "/tec_adm/coordenadorPea.ifpr", "#{breadCrumbController.navigate(4)}", "user16",
+				true, 4);
 
-		adicionarItem("Secretários", "/tec_adm/secretario.ifpr",
-				"#{breadCrumbController.navigate(5)}", "user16", true, 5);
+		adicionarItem("Secretários", "/tec_adm/secretario.ifpr", "#{breadCrumbController.navigate(5)}", "user16", true,
+				5);
 
-		adicionarItem("Estudantes", "/tec_adm/estudante.ifpr",
-				"#{breadCrumbController.navigate(10)}", "user16", true, 10);
+		adicionarItem("Estudantes", "/tec_adm/estudante.ifpr", "#{breadCrumbController.navigate(10)}", "user16", true,
+				10);
 		model.addElement(submenu);
 		submenu = new DefaultSubMenu("Gerenciar - Competição");
 
-		adicionarItem("Modalidades", "/adm/modalidade.ifpr",
-				"#{breadCrumbController.navigate(8)}", "user16", true, 8);
+		adicionarItem("Modalidades", "/adm/modalidade.ifpr", "#{breadCrumbController.navigate(8)}", "user16", true, 8);
 
-		adicionarItem("Times", "/tec_adm/time.ifpr",
-				"#{breadCrumbController.navigate(9)}", "user16", true, 9);
+		adicionarItem("Times", "/tec_adm/time.ifpr", "#{breadCrumbController.navigate(9)}", "user16", true, 9);
 
-		adicionarItem("Delegações", "/tec_adm/delegacao.ifpr",
-				"#{breadCrumbController.navigate(12)}", "attach16", true, 12);
-		
-		adicionarItem("Chaves", "/adm/chave.ifpr",
-				"#{breadCrumbController.navigate(13)}", "attach16", true, 14);
+		adicionarItem("Delegações", "/tec_adm/delegacao.ifpr", "#{breadCrumbController.navigate(12)}", "attach16", true,
+				12);
+
+		adicionarItem("Chaves", "/adm/chave.ifpr", "#{breadCrumbController.navigate(13)}", "attach16", true, 14);
+
+		adicionarItem("Locais", "/adm/local.ifpr",
+				"", "attach16", false, 15);
 		model.addElement(submenu);
 	}
 
@@ -111,29 +111,28 @@ public class MenuUsuariosViewMB {
 		this.model = model;
 	}
 
-	public void adicionarItem(String value, String url, String command,
-			String icon, boolean inSubmenu, int numeroItem) {
+	public void adicionarItem(String value, String url, String command, String icon, boolean inSubmenu,
+			int numeroItem) {
 		item = new DefaultMenuItem();
 		item.setValue(value);
 		item.setUrl(url);
-		//item.setCommand(command);
+		// item.setCommand(command);
 		item.setIcon(icon);
 		String caminhoView = context.getViewRoot().getViewId();
-		
+
 		caminhoView = caminhoView.substring(0, caminhoView.lastIndexOf("."));
 		String caminhoViewItem = url.substring(0, url.lastIndexOf("."));
-		
-		if(caminhoView.equals(caminhoViewItem)){
+
+		if (caminhoView.equals(caminhoViewItem)) {
 			item.setStyleClass("menuItemAtual");
-			if(numeroItem != 0){
+			if (numeroItem != 0) {
 				breadCrumbController.navigate(numeroItem);
-			}
-			else{
+			} else {
 				breadCrumbController.navigateHome();
 			}
-			
+
 		}
-		
+
 		if (inSubmenu) {
 			submenu.addElement(item);
 		} else {
@@ -146,8 +145,7 @@ public class MenuUsuariosViewMB {
 		return breadCrumbController;
 	}
 
-	public void setBreadCrumbController(
-			BreadCrumbController breadCrumbController) {
+	public void setBreadCrumbController(BreadCrumbController breadCrumbController) {
 		this.breadCrumbController = breadCrumbController;
 	}
 

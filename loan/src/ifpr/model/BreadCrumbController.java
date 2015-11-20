0bd51b8ea664a921b152effdda413224c0a451d9
LFVH <luizfelipevh@gmail.com>
2015-11-20 12:32:02 -0200

    asd

--- loan/src/ifpr/model/BreadCrumbController.java
+++ loan/src/ifpr/model/BreadCrumbController.java
@@ -1,105 +1,91 @@
 package ifpr.model;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
 import org.primefaces.model.menu.MenuModel;
 
+import ifpr.utils.Paths;
+
 @ManagedBean(name = "breadCrumbController")
-@SessionScoped
+@ViewScoped
 public class BreadCrumbController {
 	private MenuModel menuModel;
 	private DefaultMenuItem item;
-	private final int ARQUIVOS = 1;
-	private final int TECADM = 2;
-	private final int TECESP = 3;
-	private final int COORDENADORES = 4;
-	private final int SECRETARIOS = 5;
-	private final int CAMPUS = 6;
-	private final int NOTICIAS = 7;
-	private final int MODALIDADES = 8;
-	private final int TIMES = 9;
-	private final int ESTUDANTES = 10;
-	private final int PROJETOS = 11;
-	private final int DELEGACOES = 12;
-	private final int HORARIOASSISTENCIA = 13;
-	private final int CHAVES = 14;
+
+	FacesContext context;
 
 	public BreadCrumbController() {
 		this.menuModel = new DefaultMenuModel();
+		context = FacesContext.getCurrentInstance();
+		String caminhoView = context.getViewRoot().getViewId();
+		caminhoView = caminhoView.substring(0, caminhoView.lastIndexOf("."));
 		navigateHome();
+
+		if (caminhoView.equals((Paths.ARQUIVOS).substring(0, Paths.ARQUIVOS.lastIndexOf(".")))) {
+			adicionarItem("Gerenciar - Arquivos", Paths.ARQUIVOS);
+		} else if (caminhoView.equals((Paths.CAMPUS).substring(0, Paths.CAMPUS.lastIndexOf(".")))) {
+			adicionarItem("Gerenciar - Campus", Paths.CAMPUS);
+
+		} else if (caminhoView.equals((Paths.NOTICIAS).substring(0, Paths.NOTICIAS.lastIndexOf(".")))) {
+			adicionarItem("Gerenciar - Notícias", Paths.NOTICIAS);
+
+		} else if (caminhoView.equals((Paths.PROJETOS).substring(0, Paths.PROJETOS.lastIndexOf(".")))) {
+			adicionarItem("Gerenciar - Projetos PEA", Paths.PROJETOS);
+
+		} else if (caminhoView
+				.equals((Paths.HORARIOASSISTENCIA).substring(0, Paths.HORARIOASSISTENCIA.lastIndexOf(".")))) {
+			adicionarItem("Gerenciar - Horários de Assistências", Paths.HORARIOASSISTENCIA);
+
+		} else if (caminhoView.equals((Paths.TECADM).substring(0, Paths.TECADM.lastIndexOf(".")))) {
+			adicionarItem("Tec. Administrativos", Paths.TECADM);
+
+		} else if (caminhoView.equals((Paths.TECESP).substring(0, Paths.TECESP.lastIndexOf(".")))) {
+			adicionarItem("Tec. Esportivos", Paths.TECESP);
+
+		} else if (caminhoView.equals((Paths.COORDENADORES).substring(0, Paths.COORDENADORES.lastIndexOf(".")))) {
+			adicionarItem("Coordenadores", Paths.COORDENADORES);
+
+		} else if (caminhoView.equals((Paths.SECRETARIOS).substring(0, Paths.SECRETARIOS.lastIndexOf(".")))) {
+			adicionarItem("Secretários", Paths.SECRETARIOS);
+
+		} else if (caminhoView.equals((Paths.ESTUDANTES).substring(0, Paths.ESTUDANTES.lastIndexOf(".")))) {
+			adicionarItem("Estudantes", Paths.ESTUDANTES);
+
+		} else if (caminhoView.equals((Paths.MODALIDADES).substring(0, Paths.MODALIDADES.lastIndexOf(".")))) {
+			adicionarItem("Modalidades", Paths.MODALIDADES);
+
+		} else if (caminhoView.equals((Paths.TIMES).substring(0, Paths.TIMES.lastIndexOf(".")))) {
+			adicionarItem("Times", Paths.TIMES);
+
+		} else if (caminhoView.equals((Paths.DELEGACOES).substring(0, Paths.DELEGACOES.lastIndexOf(".")))) {
+			adicionarItem("Delegações", Paths.DELEGACOES);
+
+		} else if (caminhoView.equals((Paths.CHAVES).substring(0, Paths.CHAVES.lastIndexOf(".")))) {
+			adicionarItem("Chaves", Paths.CHAVES);
+
+		} else if (caminhoView.equals((Paths.LOCAIS).substring(0, Paths.LOCAIS.lastIndexOf(".")))) {
+			adicionarItem("Locais", Paths.LOCAIS);
+
+		}
+
 	}
 
 	public String navigateHome() {
 		menuModel.getElements().clear();
-		adicionarItem("Home", "/index.ifpr", "");
-		adicionarItem("Meu Perfil", "/user/home.ifpr", "#{breadCrumbController.navigateHome}");
+		adicionarItem("Home", "/index.ifpr");
+		adicionarItem("Meu Perfil", "/user/home.ifpr");
 
 		return "home";
 	}
 
-	public String navigate(int opcao) {
-
-		navigateHome();
-		if (opcao == ARQUIVOS) {
-			adicionarItem("Gerenciar - Arquivos", "/adm/arquivo/arquivo.loan", "#{breadCrumbController.navigate(1)}");
-			return "usuarios";
-		} else if (opcao == TECADM) {
-			adicionarItem("Gerenciar - Tec. Administrativos", "/adm/tec_adm.loan",
-					"#{breadCrumbController.navigate(2)}");
-			return "turmas";
-		} else if (opcao == TECESP) {
-			adicionarItem("Gerenciar - Tec. Esportivos", "/tec_adm/tec_esp.loan",
-					"#{breadCrumbController.navigate(3)}");
-			return "usuarios";
-		} else if (opcao == COORDENADORES) {
-			adicionarItem("Gerenciar - Coordenadores", "/tec_adm/coordenadorPea.loan",
-					"#{breadCrumbController.navigate(4)}");
-			return "usuarios";
-		} else if (opcao == SECRETARIOS) {
-			adicionarItem("Gerenciar - Secretários", "/tec_adm/secretario.loan", "#{breadCrumbController.navigate(5)}");
-			return "usuarios";
-		} else if (opcao == CAMPUS) {
-			adicionarItem("Gerenciar - Campus", "/adm/campus.loan", "#{breadCrumbController.navigate(6)}");
-			return "usuarios";
-		} else if (opcao == NOTICIAS) {
-			adicionarItem("Gerenciar - Notícias", "/adm/noticia.loan", "#{breadCrumbController.navigate(7)}");
-			return "usuarios";
-		} else if (opcao == MODALIDADES) {
-			adicionarItem("Gerenciar - Modalidades", "/adm/modalidade.loan", "#{breadCrumbController.navigate(8)}");
-			return "usuarios";
-		} else if (opcao == TIMES) {
-			adicionarItem("Gerenciar - Times", "/tec_adm/time.loan", "#{breadCrumbController.navigate(9)}");
-			return "usuarios";
-		} else if (opcao == ESTUDANTES) {
-			adicionarItem("Gerenciar - Estudantes", "/tec_adm/estudante.loan", "#{breadCrumbController.navigate(10)}");
-			return "usuarios";
-		} else if (opcao == PROJETOS) {
-			adicionarItem("Gerenciar - Projetos PEA", "/tec_adm/projeto.loan", "#{breadCrumbController.navigate(11)}");
-			return "usuarios";
-		} else if (opcao == DELEGACOES) {
-			adicionarItem("Gerenciar - Delegações", "/tec_adm/delegacao.loan", "#{breadCrumbController.navigate(12)}");
-			return "usuarios";
-		} else if (opcao == HORARIOASSISTENCIA) {
-			adicionarItem("Gerenciar - Horários de Assistências", "/coord/horarioAssistencia.loan",
-					"#{breadCrumbController.navigate(13)}");
-			return "usuarios";
-		}
-		else if (opcao == CHAVES) {
-			adicionarItem("Chaves", "/adm/chave.ifpr",
-					"#{breadCrumbController.navigate(13)}");
-			return "usuarios";
-		}
-		return "";
-	}
-
-	public void adicionarItem(String value, String url, String command) {
+	public void adicionarItem(String value, String url) {
 		item = new DefaultMenuItem();
 		item.setValue(value);
 		item.setUrl(url);
-		item.setCommand(command);
 		this.menuModel.addElement(item);
 	}
 

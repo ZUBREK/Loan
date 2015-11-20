0c11464ef4b83d1d34473d4a52588acd01eb63a5
LFVH <luizfelipevh@gmail.com>
2015-11-20 13:02:30 -0200

    comitei

---
+++ loan/src/ifpr/utils/Paths.java
@@ -0,0 +1,22 @@
+package ifpr.utils;
+
+public class Paths {
+	public static final String ARQUIVOS = "/adm/arquivo/arquivo.ifpr";
+	public static final String TECADM = "/adm/tec_adm.ifpr";
+	public static final String TECESP = "/tec_adm/tec_esp.ifpr";
+	public static final String COORDENADORES = "/tec_adm/coordenadorPea.ifpr";
+	public static final String SECRETARIOS = "/tec_adm/secretario.ifpr";
+	public static final String CAMPUS = "/adm/campus.ifpr";
+	public static final String NOTICIAS = "/adm/noticia.ifpr";
+	public static final String MODALIDADES = "/adm/modalidade.ifpr";
+	public static final String TIMES = "/tec_adm/time.ifpr";
+	public static final String ESTUDANTES = "/tec_adm/estudante.ifpr";
+	public static final String PROJETOS = "/tec_adm/projeto.ifpr";
+	public static final String DELEGACOES = "/tec_adm/delegacao.ifpr";
+	public static final String HORARIOASSISTENCIA = "/tec_adm/tec_coord/coord/horarioAssistencia.xhtml";
+	public static final String CHAVES = "/adm/chave.ifpr";
+	public static final String LOCAIS = "/adm/local.ifpr";
+	
+	public static final String INDEX = "/index.ifpr";
+	public static final String HOME = "/user/home.ifpr";
+}
